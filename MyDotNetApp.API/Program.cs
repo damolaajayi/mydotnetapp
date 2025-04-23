2609672eafaacb2d67da06b3ea8f5f23fd6185b0
Adedamola Ajayi <damolaajayi@gmail.com>
2025-04-23 20:37:49 +0100

    fix to program.cs for swagger

--- MyDotNetApp.API/Program.cs
+++ MyDotNetApp.API/Program.cs
@@ -25,11 +25,14 @@ builder.Services.AddAppDbContexts(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
